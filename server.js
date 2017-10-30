b53be346f87db18d8b19f21c075a25e1a7152107
sisbel <steveisbel01@yahoo.com>
2017-10-30 12:26:50 -0400

    Add

--- server.js
+++ server.js
@@ -14,33 +14,12 @@ app.listen(port, function() {
 });
 
 //=============================================================================
-// Creates the endpoint for our webhook
-app.post('/webhook', function(req, res) {
-
-    var body = req.body;
-
-// Checks this is an event from a page subscription
-    if (body.object === 'page') {
-
-        // Iterates over each entry - there may be multiple if batched
-        body.entry.forEach(function(entry) {
-
-            // Gets the message. entry.messaging is an array, but
-            // will only ever contain one message, so we get index 0
-            var webhookEvent = entry.messaging[0];
-            console.log(webhookEvent);
-        });
-
-        // Returns a '200 OK' response to all requests
-        res.status(200).send('EVENT_RECEIVED');
-    } else {
-        // Returns a '404 Not Found' if event is not from a page subscription
-        res.sendStatus(404);
-    }
+var received_updates = [];
+app.get('/', function(req, res) {
+    console.log(req);
+    res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
 
 });
-
-//=============================================================================
 // Adds support for GET requests to our webhook
 app.get('/facebook', function(req, res) {
 
